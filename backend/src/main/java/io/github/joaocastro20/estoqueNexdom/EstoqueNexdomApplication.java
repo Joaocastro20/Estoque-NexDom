02ce6249de52df7aea2416f51b705c6f0ee6875b
Joao Castro <joao-castro@spgg.rs.gov.br>
2025-06-17 15:54:15 -0300

    init: adiciona estrutura inicial do back-end

---
+++ backend/src/main/java/io/github/joaocastro20/estoqueNexdom/EstoqueNexdomApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaocastro20.estoqueNexdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueNexdomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueNexdomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/joaocastro20/estoqueNexdom/EstoqueNexdomApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaocastro20.estoqueNexdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueNexdomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
