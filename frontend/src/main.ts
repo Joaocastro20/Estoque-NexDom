78a1e5c6ba13ea68638f68e95538f0f8b13cbe19
Joao Castro <joao-castro@spgg.rs.gov.br>
2025-06-18 12:19:57 -0300

    feat: adicionar tabela inicial e modal salvar front

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,33 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// PrimeVue core
+import PrimeVue from 'primevue/config'
+import Dialog from 'primevue/dialog'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+
+// Estilos
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/aura-light-blue/theme.css' // ou outro tema
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+// Componentes usados
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue)
+
+app.component('DataTable', DataTable)
+app.component('Column', Column)
+app.component('Dialog', Dialog)
+app.component('Button', Button)
+app.component('InputText', InputText)
 
 app.mount('#app')
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,8 +1,90 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="card">
+    <DataTable
+      :value="customers"
+      paginator
+      :rows="5"
+      :rowsPerPageOptions="[5, 10, 20, 50]"
+      tableStyle="min-width: 50rem"
+    >
+      <Column field="descricao" header="Descrição" style="width: 25%" />
+      <Column field="codigo" header="Codigo" style="width: 25%" />
+      <Column field="tipoProduto" header="Tipo" style="width: 25%" />
+      <Column field="valorFornecedor" header="Valor" style="width: 25%" />
+      <Column header="Ações" style="width: 20%">
+        <template #body="slotProps">
+          <button @click="editCustomer(slotProps.data)" class="p-button p-button-sm p-button-text">
+            ✏️
+          </button>
+          <button @click="deleteCustomer(slotProps.data)" class="p-button p-button-sm p-button-danger p-button-text">
+            🗑️
+          </button>
+        </template>
+      </Column>
+    </DataTable>
   </div>
+
+  <Button label="Show" @click="visible = true">Novo</Button>
+
+<Dialog v-model:visible="visible" header="Salvar Novo Produto" :style="{ width: '25rem' }">
+    <span class="text-surface-500 dark:text-surface-400 block mb-8">Update your information.</span>
+    <div class="flex items-center gap-4 mb-4">
+        <label for="username" class="font-semibold w-24">Username</label>
+        <InputText id="username" class="flex-auto" autocomplete="off" />
+    </div>
+    <div class="flex items-center gap-4 mb-8">
+        <label for="email" class="font-semibold w-24">Email</label>
+        <InputText id="email" class="flex-auto" autocomplete="off" />
+    </div>
+    <div class="flex justify-end gap-2">
+        <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
+        <Button type="button" label="Save" @click="visible = false"></Button>
+    </div>
+</Dialog>
+   
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import  DataTable  from 'primevue/datatable'
+import Column from 'primevue/column'
+import axios from 'axios'
+import InputText from 'primevue/inputtext'
+
+const customers = ref([])
+
+
+
+const visible = ref(false)
+
+const form = ref({
+  username: '',
+  email: ''
+})
+
+onMounted(() => {
+  fetchCustomers();
+})
+
+async function fetchCustomers() {
+  try {
+    const response = await axios.get('http://localhost:8080/api/produtos?page=0&size=5&sort=id,asc')
+    customers.value = response.data.content
+    console.log("🚀 ~ fetchCustomers ~ response.data:", response.data)
+    
+  } catch (error) {
+    console.error('Erro ao buscar clientes:', error)
+  }
+}
+
+function editCustomer(customer) {
+  console.log('Editar:', customer)
+}
+
+function deleteCustomer(customer) {
+  console.log('Excluir:', customer)
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
