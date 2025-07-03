f22c19fed763414f660af56b468d2474f04eabaa
Joao Castro <joao-castro@spgg.rs.gov.br>
2025-07-03 08:19:04 -0300

    feat: adicionar drawer

--- frontend-angular/src/app/app.ts
+++ frontend-angular/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Home } from "./components/home/home";
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { RippleModule } from 'primeng/ripple';
+import { ViewChild } from '@angular/core';
+import { Ripple } from 'primeng/ripple';
+import { StyleClass } from 'primeng/styleclass';
+import { Drawer } from 'primeng/drawer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenubarModule],
+  imports: [RouterOutlet, MenubarModule, DrawerModule, ButtonModule, AvatarModule, RippleModule, Ripple, StyleClass, Drawer, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,23 +23,33 @@ export class App {
   itens = ["teste"]
   items: MenuItem[] = [];
 
-ngOnInit() {
-  this.items = [
-    {
-      label: 'Home',
-      icon: 'pi pi-home',
-      routerLink: '/home'
-    },
-    {
-      label: 'Produtos',
-      icon: 'pi pi-box',
-      routerLink: '/produto'
-    },
-    // {
-    //   label: 'Movimentar Produto',
-    //   icon: 'pi pi-exchange',
-    //   routerLink: '/movimentar'
-    // }
-  ];
-}
+  @ViewChild('drawerRef') drawerRef!: Drawer;
+
+  closeCallback(e: Event): void {
+    this.drawerRef.close(e);
+  }
+
+  visible: boolean = false;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        routerLink: '/home'
+      },
+      {
+        label: 'Produtos',
+        icon: 'pi pi-box',
+        routerLink: '/produto'
+      },
+      // {
+      //   label: 'Movimentar Produto',
+      //   icon: 'pi pi-exchange',
+      //   routerLink: '/movimentar'
+      // }
+    ];
+  }
+
+
 }
--- frontend-angular/src/app/components/produto/produto.ts
+++ frontend-angular/src/app/components/produto/produto.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [CardModule, PanelModule ],
   templateUrl: './produto.html',
   styleUrl: './produto.css'
 })
